89500eeda30523543cd499a3f4791d707424713f
Levochka108 <d.klochkov@gmail.com>
2023-03-27 12:10:14 +0300

    add task3

--- ProgramMain/ProgramMain.java
+++ ProgramMain/ProgramMain.java
@@ -1,11 +1,13 @@
 package ProgramMain;
 import Task1.triangleNumber;
 import Task2.factorialNumber;
+import Task3.rangeNumber;
 public class ProgramMain {
     public static void main(String[] args) {
         int n = 7;
         System.out.println("Hello");
         System.out.println(triangleNumber.triangleNum(n)); // triangle равно 28
         System.out.println(factorialNumber.factorial(n));  // factorial n равно 5040
+        rangeNumber.main(1);
     }
 }
---
+++ Task3/rangeNumber.java
@@ -0,0 +1,26 @@
+package Task3;
+
+public class rangeNumber {
+
+        public static void main(int args) {
+            for (int i = 2; i <= 1000; i++) {
+                if (isPrime(i)) {
+                    System.out.print(i + " ");
+                }
+            }
+        }
+        private static boolean isPrime(int num) {
+            if (num < 2) {
+                return false;
+            }
+            for (int i = 2; i <= Math.sqrt(num); i++) {
+                if (num % i == 0) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+
+}
+
