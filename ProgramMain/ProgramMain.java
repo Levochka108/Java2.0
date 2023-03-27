24aacdf886b3e16476a3333caa4803ed7bba4f57
Levochka108 <d.klochkov@gmail.com>
2023-03-27 12:53:29 +0300

    add task4

--- ProgramMain/ProgramMain.java
+++ ProgramMain/ProgramMain.java
@@ -2,6 +2,7 @@ package ProgramMain;
 import Task1.triangleNumber;
 import Task2.factorialNumber;
 import Task3.rangeNumber;
+import Task4.primitiveCalculate;
 public class ProgramMain {
     public static void main(String[] args) {
         int n = 7;
@@ -9,5 +10,7 @@ public class ProgramMain {
         System.out.println(triangleNumber.triangleNum(n)); // triangle равно 28
         System.out.println(factorialNumber.factorial(n));  // factorial n равно 5040
         rangeNumber.main(1);
+        System.out.println("\n");
+        primitiveCalculate.main(args);
     }
 }
---
+++ Task4/primitiveCalculate.java
@@ -0,0 +1,57 @@
+package Task4;
+import java.security.KeyStore;
+import java.util.Scanner;
+public class primitiveCalculate {
+
+
+    public static void main(String[] args) {
+        System.out.println("\uD83D\uDCBBWelcome! This is Primitive Calculate\uD83D\uDCBB!");
+
+       int flag = 1;
+        while (flag == 1) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter the first number: ");
+            double num1 = scanner.nextDouble();
+
+            System.out.print("Enter the second number: ");
+            double num2 = scanner.nextDouble();
+
+            System.out.print("Enter the operator (+, -, *, /): ");
+            char operator = scanner.next().charAt(0);
+
+            double result = 0;
+
+            switch (operator) {
+                case '+':
+                    result = num1 + num2;
+                    break;
+
+                case '-':
+                    result = num1 - num2;
+                    break;
+
+                case '*':
+                    result = num1 * num2;
+                    break;
+
+                case '/':
+                    result = num1 / num2;
+                    break;
+
+                default:
+                    System.out.printf("Error! Enter correct operator");
+                    return;
+            }
+            System.out.print("\nThe result is given as follows:\n");
+            System.out.printf(num1 + " " + operator + " " + num2 + " = " + result);
+
+            System.out.println("\nExit = 0, Next = 1: choice enter >");
+
+            int out = scanner.nextInt();
+            if (out == 0){
+                System.out.println("\n\uD83D\uDC4BSee you soon!");
+                flag = 0;
+            }
+        }
+    }
+}
